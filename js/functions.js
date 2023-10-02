42ab04ae2c20455a898c3eb2ba9ecfbc5a22417b
nenichv <alena_volkova_2004@mail.ru>
2023-10-02 16:23:54 +0500

    Исправление недочетов

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,10 @@
-const checkStringLength = function(input, maxLength) { return input.length <= maxLength };
-console.log(checkStringLength('winx', 4));
+const checkStringLength = function(input, maxLength) { return input.length <= maxLength; };
 
 const checkStringPalindrome = function(input) {
   const normalizeString = input.toLowerCase().replaceAll(' ', '');
   const reverseString = normalizeString.split('').reverse().join('');
   return normalizeString === reverseString;
 };
-console.log(checkStringPalindrome('Лёша на полке клопа нашёл'));
-console.log(checkStringPalindrome('winx club'));
 
 const extractNumbers = function(input) {
   const workString = input.toString();
@@ -16,10 +13,7 @@ const extractNumbers = function(input) {
   for (let element = 0; element < workString.length; element++) {
     const charElement = workString.at(element);
     const isNotNumber = isNaN(parseInt(charElement, 10));
-    if (!isNotNumber) resultString += charElement;
+    if (!isNotNumber) { resultString += charElement };
   }
   return resultString ? +resultString : NaN;
 };
-console.log(extractNumbers('jh ds35kj5 .90'));
-console.log(extractNumbers('empty string'));
-console.log(extractNumbers(12345));
