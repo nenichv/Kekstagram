a6b874baf6fb7abf14681d59b31346b346040d35
nenichv <alena_volkova_2004@mail.ru>
2023-11-02 23:21:37 +0500

    Отрисовка миниатюр

---
+++ js/displaying-thumbnails.js
@@ -0,0 +1,25 @@
+const patternThumbnails = document.querySelector('#picture').content.querySelector('.picture');
+const childElements = document.querySelector('.pictures');
+
+const createThumbnail = ( {url, description, likes, comments} ) => {
+  const thumbnail = patternThumbnails.cloneNode(true);
+  thumbnail.querySelector('.picture__img').src = url;
+  thumbnail.querySelector('.picture__img').alt = description;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+
+  return thumbnail;
+}
+
+const renderThumbnails = (pictures) =>{
+  const element = document.createDocumentFragment();
+
+  pictures.forEach((picture) => {
+    const thumbnail = createThumbnail(picture);
+    element.append(thumbnail);
+  });
+
+  childElements.append(element);
+}
+
+export { renderThumbnails };
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import { getPictures } from './data.js';
-
+import { renderThumbnails } from './displaying-thumbnails.js';
 getPictures();
+renderThumbnails();
