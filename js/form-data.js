a679113ece930e0884608126dd6790a7fc62f44c
nenichv <alena_volkova_2004@mail.ru>
2023-11-27 18:41:23 +0500

    Рефакторинг

--- js/form-data.js
+++ js/form-data.js
@@ -1,5 +1,5 @@
 const MAX_COUNT_HASHTAG = 5;
-const PATTERN_VALID = /^#[a-zф-яё0-9]{1,19}$/i;
+const PATTERN_VALID = "/^#[a-zа-яё0-9]{1,19}$/i";
 const Errors = {
   PATTERN_INVALID: 'Неверный хэштег!',
   UNORIGINALITY: 'Хэштеги не могут быть одинаковыми!',
@@ -14,18 +14,16 @@ const fileInput = formElement.querySelector('.img-upload__input');
 const hashtag = formElement.querySelector('.text__hashtags');
 const comment = formElement.querySelector('.text__description');
 
-const showWindow = () => {
-  overlayElement.classList.remove('hidden');
-  bodyElement.classList.add('modal-open');
-  document.addEventListener('keydown', onDocumentKeydown);
-};
+const рristine = new Pristine(formElement, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload-error',
+}, false);
 
-const hideWindow = () => {
-  formElement.reset();
-  errorPristine.reset();
-  overlayElement.classList.add('hidden');
-  bodyElement.classList.remove('modal-open');
-  document.removeEventListener('keydown', onDocumentKeydown);
+const standardizeTag = (tag) => {
+  let tagWithoutSpace = tag.trim();
+  let tagArray = tagWithoutSpace.split('');
+  tagArray.filter( (tagFromArray) => Boolean(tagFromArray.length));
 };
 
 const isValidPatternTag = (value) => {
@@ -37,16 +35,11 @@ const isValidPatternTag = (value) => {
 
 const isValidCountTag = (value) => {
   standardizeTag(value).length <= MAX_COUNT_HASHTAG; //CHECK
-}
+ }
 
 const isOriginalTag = (value) => {
   const lowerCaseTags = standardizeTag(value).map((tag) => tag.lowerCaseTags());
   return lowerCaseTags.length === new Set(lowerCaseTags).size;
-};
-
-const standardizeTag = (tag) => {
-  let tagWithoutSpace = tag.trim();
-  tagWithoutSpace.split(' ').filter( (tag) => Boolean(tag.length));
 };
 
 function onDocumentKeydown(event) {
@@ -74,44 +67,29 @@ hashtag.addEventListener('keydown', (event) => {
 
 const onFormSubmit = (event) => {
   event.preventDefault();
-  errorPristine.validate();
+  рristine.validate();
 };
 
-const onFileInputChange = () => {
-  showWindow();
+const onFileInputChange = () => showWindow();
+
+const showWindow = () => {
+  overlayElement.classList.remove('hidden');
+  bodyElement.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
 };
 
-const errorPristine = new Pristine(formElement, {
-  classTo: 'img-upload__field-wrapper',
-  errorTextParent: 'img-upload__field-wrapper',
-  errorTextClass: 'img-upload__field-wrapper-error',
-});
+const hideWindow = () => {
+  formElement.reset();
+  рristine.reset();
+  overlayElement.classList.add('hidden');
+  bodyElement.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
 
-errorPristine.addValidator(
-  hashtag,
-  isValidCountTag,
-  Errors.COUNT_INVALID,
-  3,
-  true
-);
-
-errorPristine.addValidator(
-  hashtag,
-  isValidCountTag,
-  Errors.PATTERN_INVALID,
-  2,
-  true
-);
-
-errorPristine.addValidator(
-  hashtag,
-  isOriginalTag,
-  Errors.UNORIGINALITY,
-  1,
-  true
-);
+рristine.addValidator(hashtag, isValidCountTag, Errors.COUNT_INVALID, 3, true);
+рristine.addValidator(hashtag, isValidCountTag, Errors.PATTERN_INVALID, 2, true);
+рristine.addValidator( hashtag, isOriginalTag, Errors.UNORIGINALITY, 1, true);
 
 fileInput.addEventListener('change', onFileInputChange);
 cancelButton.addEventListener('click', onCancelKeydown);
 formElement.addEventListener('submit', onFormSubmit);
-
