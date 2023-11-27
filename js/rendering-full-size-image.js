6900b0272995469d8bf52d7c4e357cd881966136
nenichv <alena_volkova_2004@mail.ru>
2023-11-27 17:03:51 +0500

    Исправила проблему с датасетом

--- js/rendering-full-size-image.js
+++ js/rendering-full-size-image.js
@@ -5,14 +5,13 @@ const pictureContainer = document.querySelector('.pictures');
 
 const renderGallery = (pictures) => {
   pictureContainer.addEventListener('click', (event) => {
-    const thumbnail = event.target.closest('[data-thumbnail-id]');//CHECK
-    const thumbnailId = parseInt(thumbnail.dataset.thumbnailId, 10);
+    const thumbnail = event.target.closest('[data-thumbnail-id]');
 
     if (!thumbnail) {
       return;
     }
     event.preventDefault();
-
+    const thumbnailId = parseInt(thumbnail.dataset.thumbnailId, 10);
     const picture = pictures.find( (item) =>
       item.id === thumbnailId
     );
