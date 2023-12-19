2829f1491ea611418ac5c692f3f3fa6c1ec51371
nenichv <alena_volkova_2004@mail.ru>
2023-12-19 19:17:22 +0500

    refactoring

--- js/type-photo.js
+++ js/type-photo.js
@@ -1,5 +1,3 @@
-const formElement = document.querySelector('.img-upload__form');
-
 export const isValidTypeFile = (file) => {
   if (file.type.startsWith('image/') || /\.(jpg|jpeg|png|gif)$/i.test(file.name)) {
     return true;
